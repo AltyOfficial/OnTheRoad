e9d8a9da78eb545d4d4f705b90dea5796cef2fef
Arthur Alty <alty.official.prim@gmail.com>
2026-07-17 13:40:36 +0300

    minor naming fix

--- auth-service/src/apps/users/infra/uow.py
+++ auth-service/src/apps/users/infra/uow.py
@@ -10,9 +10,9 @@ from src.apps.users.infra.repositories.user_session import UserSessionRepository
 class UserUnitOfWork(DatabaseUnitOfWork):
     def __init__(self, session: AsyncSession):
         super().__init__(session)
-        self._user_repository = UserRepository(model=User, db_session=session)
-        self._user_role_repository = RoleRepository(model=Role, db_session=session)
-        self._user_session_repository = UserSessionRepository(model=UserSession, db_session=session)
+        self._user_repository = UserRepository(model=User, session=session)
+        self._user_role_repository = RoleRepository(model=Role, session=session)
+        self._user_session_repository = UserSessionRepository(model=UserSession, session=session)
     
     @property
     def roles(self) -> RoleRepository:
