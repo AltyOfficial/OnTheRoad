acfa4a7dcda62d8a2889da0c1fd33b63c14b780d
Arthur Alty <alty.official.prim@gmail.com>
2026-07-17 13:40:50 +0300

    minor fix

--- auth-service/src/config/settings/app.py
+++ auth-service/src/config/settings/app.py
@@ -6,5 +6,5 @@ class AppConfig(CustomBaseSettings):
     PROJECT_V1_URL: str = "/api/v1"
     PROJECT_VERSION: str = "0.1.0"
     PROJECT_DESCRIPTION: str = "auth API"
-    PROJECT_DOCS_URL: str = "/api/docs"
+    PROJECT_DOCS_URL: str = "/api/docs/"
     PROJECT_OPENAPI_URL: str = "/api/openapi.json"
